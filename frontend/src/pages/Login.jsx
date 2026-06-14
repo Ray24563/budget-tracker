49c6d6d6d0034a2277cf8e5254d326e8703bb329
Bari <charlespalatino@gmail.com>
2026-06-14 16:30:30 +0800

    Started designing the login page. Will continue later.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,28 @@
+import { useEffect } from "react";
+import Logo from '../assets/images/logo.png'
+
 function Login() {
+  useEffect(() => {
+    document.title = "Login"
+  },[]
+)
+
   return(
     <>
-      <div className="bg-green-500">Testing lang to ah.</div>
+      <div className="h-screen flex justify-center items-center flex-col">
+
+        <div className="flex justify-center flex-col items-center gap-y-5">
+          <img src={Logo} className="w-70"/>
+          <p className="text-[#9b8ab8] -[sans-serif] text-lg">Welcome to <span className="syne-heading font-bold text-[#f0eaff]">WhyHub!</span> Please input your credential to proceed.</p>
+          <div>
+          <input 
+            className="bg-[#13102a] border border-[#3b2d6a] rounded-md px-3 w-70 py-2 text-[#f0eaff]"
+            placeholder="Enter Password"
+          />
+          <button className="px-3 py-2 bg-[#7c3aed] ms-3 rounded-sm cursor-pointer">Submit</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
