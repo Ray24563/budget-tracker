de7f2dd886e10af5ebec5202133019df57192af3
Bari <charlespalatino@gmail.com>
2026-06-18 00:10:39 +0800

    Continuation of Dashboard UI Design.

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,6 +1,6 @@
 import Logo from '../assets/images/logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faArrowRightFromBracket} from '@fortawesome/free-solid-svg-icons'
+import {faRightFromBracket} from '@fortawesome/free-solid-svg-icons'
 import FadeIn from '../components/FadeIn'
 import DayTime from '../components/DayTime.jsx'
 import { useState, useEffect } from 'react'
@@ -30,7 +30,7 @@ function Homepage ({handleLogout}){
         <div className='bg-[#e2d9f3] font-medium rounded-full px-6 py-2 text-[#13102a]'>P 1,000</div>
         <FontAwesomeIcon 
           className='text-3xl mt-1 text-[#e2d9f3] cursor-pointer hover:scale-110 transition-transform duration-500' 
-          icon={faArrowRightFromBracket} 
+          icon={faRightFromBracket} 
           onClick={()=> setLogoutModal(true)}
         />
       </div>
@@ -38,7 +38,7 @@ function Homepage ({handleLogout}){
 
     <main className='mx-10 mt-15'>
       <FadeIn>
-        <section id='section_1' className="animate-fadeIn">
+        <section id='section_1' className="animate-fadeIn mb-20">
           <h1 className='text-white syne-heading font-bold text-3xl'>
             <DayTime/>
           </h1>
@@ -57,6 +57,36 @@ function Homepage ({handleLogout}){
               <button className='bg-transparent border border-[#3b2d6a] text-[#c4b8e0] py-5 ps-7 pe-40 rounded-xl hover:bg-[#1c1640] hover:border-[#4c2f8f] cursor-pointer transition-all duration-500'><FontAwesomeIcon icon={faClipboardList} className='me-2'/> Utang Jazz</button>
             </div>
           </div>
+        </section>
+      </FadeIn>
+
+      <FadeIn>
+        <h2 className='text-[#6b5f8a] syne-heading mt-7 mb-3'>Dashboard</h2>
+        <section className='grid grid-cols-[1.8fr_1fr] gap-x-3 mb-20'>
+
+          <div className='bg-[#1c1640] border border-[#2e2460] rounded-lg px-10 py-5 text-[#6b5f8a]'>
+            Dito ang Expenses Bar Graph for the Month.
+          </div>
+
+          <div className='bg-[#1c1640] border border-[#2e2460] rounded-lg px-10 py-5 text-[#6b5f8a]'>
+            Dito ang Expenses vs Income Total For Month
+          </div>
+
+        </section>
+      </FadeIn>
+
+      <FadeIn>
+        <h2 className='text-[#6b5f8a] syne-heading mt-7 mb-3'>Recent Transaction</h2>
+        <section className='grid grid-cols-2 gap-x-3'>
+
+          <div className='bg-[#1c1640] border border-[#2e2460] rounded-lg px-10 py-5 text-[#6b5f8a]'>
+            Recent Income
+          </div>
+
+          <div className='bg-[#1c1640] border border-[#2e2460] rounded-lg px-10 py-5 text-[#6b5f8a]'>
+            Recent Expenses
+          </div>
+
         </section>
       </FadeIn>
      
